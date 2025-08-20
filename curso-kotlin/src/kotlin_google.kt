5529c8b6cc5737a972a66afd3c5c183b7a968b7c
Andres Timote <pipetimoty@gmail.com>
2025-08-19 23:22:42 -0500

    Agregue mas ejercicios del curso, mas que todo los que estan de penultimos.

--- curso-kotlin/src/kotlin_google.kt
+++ curso-kotlin/src/kotlin_google.kt
@@ -3,12 +3,27 @@
     println("Hola, Andres Timote")
 }*/
 
+/*fun main() {
+    println("Tuesday")
+    println("Thursday")
+    println("Wednesday")
+    println("Friday")
+    println("Monday")
+}*/
+
 /*fun main(){
     println("Use the val keyword when the value doesn't change.")
     println("Use the var keyword when the value can change.")
     println("When you define a function, you define the parameters that can be passed to it.")
     println("When you call a function, you pass arguments for the parameters.")
 }*/
+
+/*
+fun main(){
+    val count: Int = 10
+    println("You have $count unread messages. ")
+}
+*/
 
 /*
 fun main(){
@@ -19,4 +34,36 @@ fun main(){
     offer = "Sale - Up to $discountPercentage% discount on $item! Hurry up!"
 
     println(offer)
+}*/
+
+/*
+fun main() {
+    val unreadCount = 5
+    val readCount = 100
+    println("You have ${unreadCount + readCount} total messages in your inbox.")
+}*/
+
+/*
+fun main() {
+    val numberOfPhotos = 100
+    val photosDeleted = 10
+    println("$numberOfPhotos photos")
+    println("$photosDeleted photos deleted")
+    println("${numberOfPhotos - photosDeleted} photos left")
+}*/
+
+/*
+fun main() {
+    var count = 10
+    println("You have $count unread messages.")
+    count--
+    println("You have $count unread messages.")
+}*/
+
+/*
+fun main() {
+    val numberOfAdults = 20
+    val numberOfKids = 30
+    val total = numberOfAdults + numberOfKids
+    println("The total party size is: $total")
 }*/
